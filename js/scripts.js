ac54c666a05f7cf97e95f38427b885f648eba247
joswald123 <julianaandreaoswald@gmail.com>
2022-06-25 14:28:39 -0700

    created a function that formatting the date. added more comments to the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//Global variables 
+//Global variables
 const gallery = document.getElementById("gallery");
 const modalDiv = document.createElement("div");
 //Var that creates the modal-container
@@ -15,14 +15,20 @@ fetch("https://randomuser.me/api/?results=12")
   .then((data) => generateHTML(data.results));
 
 // ------------------------------------------
-//  HELPER FUNCTIONS 
+//  HELPER FUNCTIONS
 //  Display info for Galley div & ModalDiv
 // ------------------------------------------
+
+/**
+ * generateHTML() function
+ * @param(data) response from API 
+ * @returns(element DOM) creates a literal with all the API info
+ */
 function generateHTML(data) {
-    employeesData = data;
-    let html = "";
-    employeesData.map((employee, index) => {
-      html = `<div class="card" data-index="${index}">
+  employeesData = data;
+  let html = "";
+  employeesData.map((employee, index) => {
+    html = `<div class="card" data-index="${index}">
                   <div class="card-img-container">
                       <img class="card-img" src="${employee.picture.large}" alt="profile picture">
                   </div>
@@ -32,23 +38,33 @@ function generateHTML(data) {
                       <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
                   </div>
               </div>`;
-      gallery.insertAdjacentHTML("beforeend", html);
-    });
-  
-    return;
-  }
+    gallery.insertAdjacentHTML("beforeend", html);
+  });
 
-  function generateModal(index) {
-    let {
-      name,
-      email,
-      cell,
-      dob,
-      picture,
-      location: { city, state, postcode, street },
-    } = employeesData[index];
-  
-    let html = `<div class="modal">
+  return;
+}
+
+/**
+ * generateModaL() function
+ * @param(number) index of a card
+ * @returns(element DOM) creates a literal modal that display more info of each card previously created
+ */
+function generateModal(index) {
+  // employee object destructing
+  let {
+    name,
+    email,
+    cell,
+    dob,
+    picture,
+    location: { city, state, postcode, street },
+  } = employeesData[index];
+
+  // formatting dob date
+  const birthday = formatDate(dob.date);
+
+  // Creates the literal template for the modal with the employee object destructing info
+  let html = `<div class="modal">
                   <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                   <div class="modal-info-container">
                       <img class="modal-img" src="${picture.large}" alt="profile picture">
@@ -58,26 +74,38 @@ function generateHTML(data) {
                       <hr>
                       <p class="modal-text">${cell}</p>
                       <p class="modal-text">${street.number} ${street.name}, ${state}, ${postcode}</p>
-                      <p class="modal-text">Birthday: ${dob.date}</p>
+                      <p class="modal-text">Birthday: ${birthday}</p>
                   </div>
               </div>`;
-    
-    modalDiv.innerHTML = html;
-    modalDiv.style.display = "block";
 
-    return;
-  }
+  modalDiv.innerHTML = html;
+  modalDiv.style.display = "block";
+
+  return;
+}
+
+/**
+ * formatDate function
+ * @param(string) date as a string
+ * @returns(date) formatting date
+ */
+function formatDate(date) {
+  const day = new Date(date).getDate();
+  const month = new Date(date).getMonth();
+  const year = new Date(date).getFullYear();
+  return `${month}/${day}/${year}`;
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
 // Each time a card is clicked show a modal card with more info of the card
 gallery.addEventListener("click", function (e) {
-    const card = e.target.closest(".card");
-    const index = card.getAttribute("data-index");
-    generateModal(index);
-    const button = document.querySelector("#modal-close-btn")
-    button.addEventListener("click", function() {
-        modalDiv.style.display = "none";
-    })
+  const card = e.target.closest(".card");
+  const index = card.getAttribute("data-index");
+  generateModal(index);
+  const button = document.querySelector("#modal-close-btn");
+  button.addEventListener("click", function () {
+    modalDiv.style.display = "none";
   });
+});
